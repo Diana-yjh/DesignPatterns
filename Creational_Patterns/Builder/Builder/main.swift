b55c157df4ff6433741338d8462474cad62ee576
florence96 <projin1234@gmail.com>
2023-11-23 15:46:57 +0900

    [Add] Creational Patterns added

---
+++ Creational_Patterns/Builder/Builder/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Builder
+//
+//  Created by Yejin Hong on 11/23/23.
+//
+
+import Foundation
+
+print("Hello, World!")
+
---
+++ Creational_Patterns/Singletone/Singletone/main.swift
@@ -0,0 +1,11 @@
+//
+//  main.swift
+//  Singletone
+//
+//  Created by Yejin Hong on 11/23/23.
+//
+
+import Foundation
+
+print("Hello, World!")
+
